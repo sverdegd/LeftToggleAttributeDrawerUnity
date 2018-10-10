d7aa22f1129911acbb9de2f33aff2b6280204e37
Sergio Verde <43314691+sverdegd@users.noreply.github.com>
2018-10-10 10:42:08 +0200

    Initial commit

---
+++ Assets/Utils/LeftToggleAttribute.cs
@@ -0,0 +1,18 @@
+﻿//LeftToggleAttributeDrawer by SVerde
+//contact@sverdegd.com
+
+using UnityEngine;
+
+public class LeftToggleAttribute : PropertyAttribute
+{
+    public string labelOverride = string.Empty;
+
+    public LeftToggleAttribute(){
+        
+    }
+
+    public LeftToggleAttribute(string labelOverride)
+    {
+        this.labelOverride = labelOverride;
+    }
+}
---
+++ Assets/Utils/LeftToggleAttributeDrawer.cs
@@ -0,0 +1,27 @@
+﻿//LeftToggleAttributeDrawer by SVerde
+//contact@sverdegd.com
+
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(LeftToggleAttribute))]
+public class LeftToggleAttributeDrawer : PropertyDrawer
+{
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        position.height = 16;
+        if (property.propertyType == SerializedPropertyType.Boolean) {
+            LeftToggleAttribute leftToggle = attribute as LeftToggleAttribute;
+
+            if (leftToggle.labelOverride == string.Empty)
+                EditorGUI.ToggleLeft(position, label, property.boolValue);
+            else
+                EditorGUI.ToggleLeft(position, leftToggle.labelOverride, property.boolValue);
+        }
+        else {
+            EditorGUI.LabelField(position, label.text, "Use LeftToggle with bool");
+        }
+
+    }
+}
