f09f10f5581c541edaba41f0c90b1820933b260f
Sergio Verde <43314691+sverdegd@users.noreply.github.com>
2018-10-11 09:53:35 +0200

    Bug fix

--- Assets/Utils/LeftToggleAttributeDrawer.cs
+++ Assets/Utils/LeftToggleAttributeDrawer.cs
@@ -15,9 +15,9 @@ public class LeftToggleAttributeDrawer : PropertyDrawer
             LeftToggleAttribute leftToggle = attribute as LeftToggleAttribute;
 
             if (leftToggle.labelOverride == string.Empty)
-                EditorGUI.ToggleLeft(position, label, property.boolValue);
+                property.boolValue = EditorGUI.ToggleLeft(position, label, property.boolValue);
             else
-                EditorGUI.ToggleLeft(position, leftToggle.labelOverride, property.boolValue);
+                property.boolValue = EditorGUI.ToggleLeft(position, leftToggle.labelOverride, property.boolValue);
         }
         else {
             EditorGUI.LabelField(position, label.text, "Use LeftToggle with bool");
